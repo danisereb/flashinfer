6d24cc8977fdd285530f2ef2547c4170577cd0c8
Daniel Serebrenik <daserebrenik@nvidia.com>
2026-02-02 17:49:41 +0200

    Add more cluster shapes for auto-tune, test_mm_mxfp8 works

--- include/flashinfer/gemm/mxfp8_gemm_cutlass_template.h
+++ include/flashinfer/gemm/mxfp8_gemm_cutlass_template.h
@@ -203,10 +203,10 @@ std::vector<CutlassGemmConfig> CutlassMxfp8GemmRunner<T, mxfp8GemmType>::getConf
   };
 
   std::vector<ClusterShape> clusterShapes = {
-      ClusterShape::ClusterShape_1x1x1,
-      ClusterShape::ClusterShape_1x2x1,
-      ClusterShape::ClusterShape_2x1x1,
-      ClusterShape::ClusterShape_2x2x1,
+      ClusterShape::ClusterShape_1x1x1, ClusterShape::ClusterShape_1x2x1,
+      ClusterShape::ClusterShape_2x1x1, ClusterShape::ClusterShape_2x2x1,
+      ClusterShape::ClusterShape_1x4x1, ClusterShape::ClusterShape_2x4x1,
+      ClusterShape::ClusterShape_4x2x1, ClusterShape::ClusterShape_4x4x1,
   };
   for (auto const& tile_config : tilesSm100) {
     for (auto const& cluster_config : clusterShapes) {
