5fe26e242abd2879f9b87b02d70b7cc33a8f2e40
Daniel Serebrenik <daserebrenik@nvidia.com>
2026-02-02 17:13:04 +0200

    Add more tile shapes for auto-tune

--- include/flashinfer/gemm/mxfp8_gemm_cutlass_template.h
+++ include/flashinfer/gemm/mxfp8_gemm_cutlass_template.h
@@ -196,6 +196,7 @@ std::vector<CutlassGemmConfig> CutlassMxfp8GemmRunner<T, mxfp8GemmType>::getConf
   std::vector<CutlassGemmConfig> candidateConfigs;
 
   std::vector<CutlassTileConfigSM100> tilesSm100 = {
+      CutlassTileConfigSM100::CtaShape128x64x128B,
       CutlassTileConfigSM100::CtaShape128x256x128B,
       CutlassTileConfigSM100::CtaShape128x128x256B,
       CutlassTileConfigSM100::CtaShape128x256x256B,
